794f0c8f12dfa721d2b875ab98ea1f1f918730f1
Andre Belanger <andrebelanger@me.com>
2015-03-03 20:06:57 -0500

    Clicking on a list item in MainActivity now launches CourseDetailActivity, and prints out the associated course's name.

---
+++ app/src/main/java/group5/caniskipclass/CourseDetailActivity.java
@@ -0,0 +1,49 @@
+package group5.caniskipclass;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class CourseDetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_detail);
+
+        int position = getIntent().getExtras().getInt("position");
+
+        //System.out.println(position);
+        CourseList courseList = CourseList.getInstance(this);
+        String courseName = courseList.getCourseNames().get(position);
+        TextView courseNameView = (TextView) findViewById(R.id.course_name);
+        courseNameView.setText(courseName);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_course_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
